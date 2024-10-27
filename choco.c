829d14aa4794e0c6f342312ecbe3f427d777e193
Mintsirup <140862658+Mintsirup@users.noreply.github.com>
2024-10-27 19:55:06 +0900

    Add files via upload

---
+++ choco.c
@@ -0,0 +1,13 @@
+#include <pxa255.c>
+#include <time.h>
+#include <gpio.h>
+#include <stdio.h>
+#include <string.h>
+
+inst main(viod)
+{
+	while(1){
+		printf("hello world\n");
+		msleep(1000);
+	}
+}
